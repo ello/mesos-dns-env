3a6ec0a617548168af31fa5ddf9cceab08c59e8b
Jay Zeschin <jay.zeschin@modeset.com>
2015-08-05 17:29:28 -0600

    Source Dockerfile puts mesos-dns in /

--- mesos-dns.sh
+++ mesos-dns.sh
@@ -2,4 +2,4 @@
 DOMAIN="${MESOS_DNS_DOMAIN:-mesos}"
 RESOLVERS=$(cat /etc/resolv.conf | sed -n '/^nameserver/p' | sed -e 's/nameserver\ //g' | sed -n '/127.0.0.1/!p' | paste -s -d"," -)
 cat config-template.json | jq ".zk = \"${ZK}\" | .domain = \"${DOMAIN}\" | .resolvers = (\"${RESOLVERS}\" | split(\",\"))" > config.json
-/usr/bin/mesos-dns -v 2 -config=config.json
+/mesos-dns -v 2 -config=config.json
